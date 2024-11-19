59fdac6edd131f61932f270b828f37c0fedb51ae
손해솔 (주) BD <hsson@bluedigm.com>
2024-11-19 12:18:53 +0900

    refactoring

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,9 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { checkGuess } from "../../game-helpers";
+import GuessResults from '../GuessResults'
 import Keyboard from "../Keyboard";
+
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,52 +14,59 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+
   const [keyword, setKeyword] = React.useState('');
   const [guessList, setGuessList] = React.useState([]);
   const [userStatus, setUserStatus] = React.useState('playing');
 
+  const handleSubmit = (e) => {
+    let playStatus = userStatus;
+    e.preventDefault();
+    setKeyword('')
+    if (guessList.length < NUM_OF_GUESSES_ALLOWED) {
+      let correct = true;
+      const guess = checkGuess(keyword, answer);
+      setGuessList([...guessList, guess]);
+      Array.from(guess).forEach((letterObj, i) => {
+        if (letterObj.status !== 'correct') {
+          correct = false;
+          return;
+        }
+      })
+      if (correct) {
+        playStatus = 'won'
+        setUserStatus(playStatus);
+      }
+    }
+    if (guessList.length + 1 === NUM_OF_GUESSES_ALLOWED && playStatus !== 'won') {
+      playStatus = 'lost'
+      setUserStatus(playStatus)
+    }
+  }
+
+  const happyBanner = (
+      <div className="happy banner" style={{display: userStatus === 'won' ? 'block' : 'none'}}>
+        <p>
+          <strong>Congratulations!</strong> Got it in
+          <strong>{guessList.length} guesses</strong>.
+        </p>
+      </div>
+  );
+
+  const sadBanner = (
+      <div className="sad banner" style={{display: userStatus === 'lost' ? 'block' : 'none'}}>
+        <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+      </div>
+  );
+
+
+
   return (<>
-    <div className={"guess-results"}>
-      {guessList.map((guess) => {
-        return (<p key={Math.random()} className={"guess"}>
-          {guess.map((letterObj, i) => {
-            return (<span key={i} className={`cell ${letterObj.status}`}>{letterObj.letter}</span>);
-          })}
-        </p>)
-      })}
-      {Array.from({length: NUM_OF_GUESSES_ALLOWED - guessList.length}, (_, i) => {
-        return (<p key={i} className={"guess"}>
-          {Array.from({length: 5}, (_, j) => {
-            return (<span key={j} className={"cell"}></span>);
-          })}
-        </p>)
-      })}
-    </div>
+    <GuessResults guessList={guessList} />
     <Keyboard guessList={guessList} />
     <form
         className={"guess-input-wrapper"}
-        onSubmit={(e) => {
-          e.preventDefault();
-          setKeyword('')
-          if (guessList.length < NUM_OF_GUESSES_ALLOWED) {
-            let correct = true;
-            const guess = checkGuess(keyword, answer);
-            setGuessList([...guessList, guess]);
-            Array.from(guess).forEach((letterObj, i) => {
-              if (letterObj.status !== 'correct') {
-                correct = false;
-                return;
-              }
-            })
-            if (correct) {
-              setUserStatus('won');
-            }
-          }
-          if (guessList.length + 1 === NUM_OF_GUESSES_ALLOWED && userStatus !== 'won') {
-            setUserStatus('lost')
-          }
-        }}
-    >
+        onSubmit={(e) => handleSubmit(e)}>
       <label htmlFor={"guess-input"}>Enter guess: </label>
       <input
           id={"guess-input"}
@@ -69,15 +78,9 @@ function Game() {
           disabled={userStatus !== 'playing'}
       />
     </form>
-    <div className="happy banner" style={{display: userStatus === 'won' ? 'block' : 'none'}}>
-      <p>
-        <strong>Congratulations!</strong> Got it in
-        <strong>{guessList.length} guesses</strong>.
-      </p>
-    </div>
-    <div className="sad banner" style={{display: userStatus === 'lost' ? 'block' : 'none'}}>
-      <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
-    </div>
+
+    {happyBanner}
+    {sadBanner}
   </>);
 }
 
---
+++ src/components/GuessResults/GuessResults.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
+
+function GuessResults({ guessList }) {
+  return (
+      <div className={"guess-results"}>
+          {guessList.map((guess) => {
+              return (<p key={Math.random()} className={"guess"}>
+                  {guess.map((letterObj, i) => {
+                      return (<span key={i} className={`cell ${letterObj.status}`}>{letterObj.letter}</span>);
+                  })}
+              </p>)
+          })}
+          {Array.from({length: NUM_OF_GUESSES_ALLOWED - guessList.length}, (_, i) => {
+              return (<p key={i} className={"guess"}>
+                  {Array.from({length: 5}, (_, j) => {
+                      return (<span key={j} className={"cell"}></span>);
+                  })}
+              </p>)
+          })}
+      </div>
+  );
+}
+export default GuessResults;
